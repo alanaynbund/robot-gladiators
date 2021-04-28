945843788c8dd89acf13db507916d9309ccbd3f3
Stanley <aynbunda@gmail.com>
2021-04-28 14:02:53 -0400

    robot name and stuff

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+var playerName = window.prompt("What is your robots name?");
+
+console.log(playerName);
+
+console.log(10+10);
+
+console.log ("Our Robot's name is" + playerName);
+
+function fight () {
+    window.alert ("The fight has begun!");
+}
+
+// fight ()
